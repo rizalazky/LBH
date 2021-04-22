0b8d3a06a4da2950ae425f2353364bd1dd72d1fd
rizalazky <rizalazky26@gmail.com>
2021-04-22 13:42:25 +0700

    test

--- app/Helpers/PostToNetsuite_helper.php
+++ app/Helpers/PostToNetsuite_helper.php
@@ -163,9 +163,7 @@ function getAllLocation(){
       CURLOPT_FOLLOWLOCATION => true,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => 'GET',
-      CURLOPT_POSTFIELDS =>'{
-        "phone_number": "'.$noHp.'"
-    }',
+      CURLOPT_POSTFIELDS =>'',
       CURLOPT_HTTPHEADER => array(
         $header,
         'Content-Type: application/json'
